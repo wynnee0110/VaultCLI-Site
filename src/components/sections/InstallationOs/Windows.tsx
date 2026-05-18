ec76ff732ad41a1bb6e8d7ea7e641beef73e0c57
Wayne <wynnee0110@gmail.com>
2026-05-18 20:09:00 +0800

    upd

--- src/components/sections/InstallationOs/Windows.tsx
+++ src/components/sections/InstallationOs/Windows.tsx
@@ -38,9 +38,41 @@ function Windows() {
           <p className="p-2 text-sm text-zinc-400">
             Copy and paste the following command into PowerShell:
           </p>
+<div className="w-full p-4 bg-zinc-900 rounded-xl overflow-x-auto">
+  <pre className="text-sm text-zinc-300">
+    <code className="whitespace-pre-wrap break-all">
+      irm https://raw.githubusercontent.com/wynnee0110/VaultCLI/main/install.ps1 | iex
+    </code>
+  </pre>
+</div>
+   
           </div>
         </div>
       </div>
+
+      {/* STEP 3 */}
+      <div className="flex items-start gap-4 mt-12">
+        <div className={stepNumberClass}>3</div>
+
+        <div className="flex-1">
+          <div>
+          <h1 className="text-md p-2">Configure VaultCLI</h1>
+          <p className="p-2 text-sm text-zinc-400">
+            in the terminal type the following command:
+            <div className="w-full p-4 bg-zinc-900 rounded-xl overflow-x-auto">
+  <pre className="text-sm text-zinc-300">
+    <code className="whitespace-pre-wrap break-all">
+      <span className="text-green-500">$ vault </span>init
+    </code>
+  </pre>
+</div>
+          </p>
+          </div>
+        </div>
+      </div>
+
+
+
     </div>
   );
 }
